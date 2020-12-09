c1f6645e89aafcca3b4fe995bb26dbbc115792b1
Sükein Alex <sukein.alex@gmail.com>
2020-12-09 21:33:21 +0100

    add another solution for 2020 day1 part2

--- src/test/scala/y2020/Day1.scala
+++ src/test/scala/y2020/Day1.scala
@@ -65,6 +65,15 @@ class Day1 extends AnyWordSpecLike with Matchers {
     filtered.head
   }
 
+  def day1part2try2(input: Seq[String]): Int = {
+    input
+      .map(_.toInt)
+      .combinations(3)
+      .find(_.sum == 2020)
+      .map(_.product)
+      .get
+  }
+
   "Part 1" should {
     "solve the example" in {
       val input = InputReader.getExample(2020, 1)
@@ -83,11 +92,13 @@ class Day1 extends AnyWordSpecLike with Matchers {
     "solve the example" in {
       val input = InputReader.getExample(2020, 1)
       day1part2(input) shouldBe 241861950
+      day1part2try2(input) shouldBe 241861950
     }
 
     "solve the task" in {
       val input = InputReader.getTask(2020, 1)
       day1part2(input) shouldBe 176647680
+      day1part2try2(input) shouldBe 176647680
     }
   }
 }
