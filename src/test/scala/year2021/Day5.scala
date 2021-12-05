7d816a0bc0b0da2d808cc85eac6ff78d301f8cfa
Sükein Alex <sukein.alex@gmail.com>
2021-12-05 18:17:30 +0100

    solve 2021 day5 part2

--- src/test/scala/year2021/Day5.scala
+++ src/test/scala/year2021/Day5.scala
@@ -6,9 +6,7 @@ import util.{Logger, Task}
 import year2021.day5.Day5Util._
 
 class Day5 extends Task(2021, 5) with AnyWordSpecLike with Matchers {
-  def part1(input: Seq[String]): Int = {
-    implicit val log: Logger = new Logger(false)
-
+  def part1(input: Seq[String])(implicit log: Logger): Int = {
     val dangerousFields: Seq[(Int, Int)] = getDangerousFields(input)
     val dangerLevels                     = accumulateDangerousFields(dangerousFields)
 
@@ -16,15 +14,46 @@ class Day5 extends Task(2021, 5) with AnyWordSpecLike with Matchers {
       .count(_._2 > 1)
   }
 
+  def part2(input: Seq[String])(implicit log: Logger): Int = {
+    val (xMax, yMax)                       = getXY(input)
+    val dangerousFields: Seq[(Int, Int)]   = getDangerousFields(input, withDiagonals = true)
+    val dangerLevels: Map[(Int, Int), Int] = accumulateDangerousFields(dangerousFields)
+
+    printDangerLevels(dangerLevels, xMax, yMax)
+
+    dangerLevels
+      .count(_._2 > 1)
+  }
+
   "Part 1" should {
     "solve the example" in {
+      implicit val log: Logger = new Logger(true)
+
       val input = getExample()
       part1(input) shouldBe 5
     }
 
     "solve the task" in {
+      implicit val log: Logger = new Logger(false)
+
       val input = getTask()
       part1(input) shouldBe 3990
+    }
+  }
+
+  "Part 2" should {
+    "solve the example" in {
+      implicit val log: Logger = new Logger(true)
+
+      val input = getExample()
+      part2(input) shouldBe 12
+    }
+
+    "solve the task" in {
+      implicit val log: Logger = new Logger(false)
+
+      val input = getTask()
+      part2(input) shouldBe 21305
     }
   }
 }
--- src/test/scala/year2021/day5/Day5Util.scala
+++ src/test/scala/year2021/day5/Day5Util.scala
@@ -2,8 +2,26 @@ package year2021.day5
 
 import util.Logger
 
+import scala.math.max
+
 object Day5Util {
-  def getDangerousFields(input: Seq[String])(implicit log: Logger): Seq[(Int, Int)] = {
+  def getXY(input: Seq[String])(implicit log: Logger): (Int, Int) = {
+    log.info("====================")
+    val (xMax, yMax) = input
+      .foldLeft((0, 0)) {
+        case ((xMax, yMax), next) =>
+          next match {
+            case s"$x1s,$y1s -> $x2s,$y2s" =>
+              val (x1, x2, y1, y2) = (x1s.toInt, x2s.toInt, y1s.toInt, y2s.toInt)
+              (max(max(x1, x2), xMax), max(max(y1, y2), yMax))
+          }
+      }
+    log.info(s"Size of the field is $xMax x $yMax")
+    log.info("====================")
+    (xMax, yMax)
+  }
+
+  def getDangerousFields(input: Seq[String], withDiagonals: Boolean = false)(implicit log: Logger): Seq[(Int, Int)] = {
     log.info("====================")
     val ret = input.flatMap(line => {
       line match {
@@ -17,6 +35,30 @@ object Day5Util {
             } else if (y1 == y2) {
               for (x <- orderedRangeOf(x1, x2)) yield {
                 (x, y1)
+              }
+            } else if (withDiagonals && x1 - x2 == y2 - y1) {
+              if (x1 < x2) {
+                // 5,5 -> 8,2
+                for (i <- 0 to (x2 - x1)) yield {
+                  (x1 + i, y1 - i)
+                }
+              } else {
+                // 8,0 -> 0,8
+                for (i <- 0 to (x1 - x2)) yield {
+                  (x1 - i, y1 + i)
+                }
+              }
+            } else if (withDiagonals && x1 - x2 == y1 - y2) {
+              if (x1 < x2) {
+                // 0,0 -> 8,8
+                for (i <- 0 to (x2 - x1)) yield {
+                  (x1 + i, y1 + i)
+                }
+              } else {
+                // 6,4 -> 2,0
+                for (i <- 0 to (x1 - x2)) yield {
+                  (x1 - i, y1 - i)
+                }
               }
             } else {
               Seq.empty[(Int, Int)]
@@ -48,5 +90,14 @@ object Day5Util {
     log.info(s"Accumulated dangers are: $accumulated")
     log.info("====================")
     accumulated
+  }
+
+  def printDangerLevels(dangerLevels: Map[(Int, Int), Int], xMax: Int, yMax: Int)(implicit log: Logger): Unit = {
+    for (y <- 0 to yMax) yield {
+      val fieldLine = (for (x <- 0 to xMax) yield {
+        dangerLevels.getOrElse((x, y), ".").toString
+      }).mkString
+      log.info(fieldLine)
+    }
   }
 }
