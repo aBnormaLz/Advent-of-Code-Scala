ab32a028ccedcef75f25124b38f311181268bbcb
Sükein Alex <sukein.alex@gmail.com>
2021-12-05 19:25:11 +0100

    refactor

--- src/test/scala/year2021/Day4.scala
+++ src/test/scala/year2021/Day4.scala
@@ -3,25 +3,182 @@ package year2021
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpecLike
 import util.{Printer, Task}
-import year2021.day4.BingoUtil
 
 class Day4 extends Task(2021, 4) with AnyWordSpecLike with Matchers {
   implicit val printer: Printer = new Printer(true)
 
   def part1(input: Seq[String]): Int = {
     val allWinningNumbers              = input.head.split(",").map(_.toInt)
-    val rawTables                      = BingoUtil.parseBingos(input)
-    val (winningTable, lastDrewNumber) = BingoUtil.findWinningTableAndLastNumber(rawTables, allWinningNumbers)
+    val rawTables                      = parseBingos(input)
+    val (winningTable, lastDrewNumber) = findWinningTableAndLastNumber(rawTables, allWinningNumbers)
 
     winningTable.sumOfNotWinningNumbers() * lastDrewNumber
   }
 
   def part2(input: Seq[String]): Int = {
     val allWinningNumbers              = input.head.split(",").map(_.toInt)
-    val rawTables                      = BingoUtil.parseBingos(input)
-    val (winningTable, lastDrewNumber) = BingoUtil.findLastWinningTableAndLastNumber(rawTables, allWinningNumbers)
+    val rawTables                      = parseBingos(input)
+    val (winningTable, lastDrewNumber) = findLastWinningTableAndLastNumber(rawTables, allWinningNumbers)
 
     winningTable.sumOfNotWinningNumbers() * lastDrewNumber
+  }
+
+  case class BingoNumber(number: Int, isWining: Boolean)
+
+  object BingoNumber {
+    def isLineWinning(line: Seq[BingoNumber]): Boolean = {
+      line.forall(_.isWining)
+    }
+  }
+  import BingoNumber._
+
+  case class BingoTable(numbers: Seq[Seq[Int]], drewNumbers: Seq[Int]) {
+    def prettyToString(): String = {
+      numbers.map { line =>
+        line.map { number =>
+          val padded = number.toString.reverse.padTo(2, " ").reverse.mkString("")
+          if (drewNumbers.contains(number)) {
+            val ret = s"($padded)"
+            ret
+          } else {
+            val ret = s" $padded "
+            ret
+          }
+        }.mkString(" ")
+      }.mkString("\n") + "\n"
+    }
+
+    def toWinningTable(): Seq[Seq[BingoNumber]] = {
+      numbers.map { line =>
+        line.map { number =>
+          BingoNumber(number, drewNumbers.contains(number))
+        }
+      }
+    }
+
+    def isWinning(): Boolean = {
+      val winningTable    = toWinningTable()
+      val isAnyRowWinning = winningTable.exists(isLineWinning)
+      val isAnyColWinning = winningTable.transpose.exists(isLineWinning)
+
+      isAnyRowWinning || isAnyColWinning
+    }
+
+    def sumOfNotWinningNumbers(): Int = {
+      toWinningTable()
+        .map(line => {
+          line
+            .filterNot(_.isWining)
+            .map(_.number)
+            .sum
+        })
+        .sum
+    }
+  }
+
+  def parseBingos(input: Seq[String])(implicit printer: Printer): Seq[Seq[Seq[Int]]] = {
+    val numberOfBingos = (input.length - 1) / 6
+    printer.printLine(s"numberOfBingos: $numberOfBingos")
+    printer.printLine()
+
+    for (i <- 0 until numberOfBingos) yield {
+      val startOfBingo = (i * 6) + 2
+      Seq(
+        input(startOfBingo),
+        input(startOfBingo + 1),
+        input(startOfBingo + 2),
+        input(startOfBingo + 3),
+        input(startOfBingo + 4),
+      )
+        .map(_.trim.split(" +").toSeq)
+        .map(_.map(rawNumber => rawNumber.toInt))
+    }
+  }
+
+  def findWinningTableAndLastNumber(
+      rawTables: Seq[Seq[Seq[Int]]],
+      allWinningNumbers: Seq[Int],
+  )(implicit printer: Printer): (BingoTable, Int) = {
+    var i                        = 1
+    var winningTableFound        = false
+    var winningTable: BingoTable = null
+
+    while (!winningTableFound) {
+      printer.printLine("====================")
+      val drewNumbers = allWinningNumbers.slice(0, i)
+      printer.printLine(s"Drew numbers: ${drewNumbers.mkString(", ")}")
+      printer.printLine()
+      rawTables.foreach(rawTable => {
+        val bingoTable = BingoTable(rawTable, drewNumbers)
+        printer.printLine(bingoTable.prettyToString())
+      })
+
+      val winningTables = rawTables
+        .map(rawTable => BingoTable(rawTable, drewNumbers))
+        .filter(_.isWinning())
+
+      winningTables match {
+        case Seq(table) =>
+          winningTableFound = true
+          winningTable = table
+          printer.printLine("Winning table found!!")
+          printer.printLine()
+          printer.printLine(winningTable.prettyToString())
+        case Seq(_, _*) =>
+          throw new IllegalStateException("More than one winning table found!")
+        case _          =>
+          i = i + 1
+          printer.printLine("No winning table found...")
+      }
+      printer.printLine("====================")
+    }
+
+    (winningTable, allWinningNumbers(i - 1))
+  }
+
+  def findLastWinningTableAndLastNumber(
+      rawTables: Seq[Seq[Seq[Int]]],
+      allWinningNumbers: Seq[Int],
+  )(implicit printer: Printer): (BingoTable, Int) = {
+    var i                                 = 1
+    var lastWinningTableFound             = false
+    var lastWinningTable: BingoTable      = null
+    var nonWinningTables: Seq[BingoTable] = rawTables
+      .map(rawTable => BingoTable(rawTable, Seq()))
+
+    while (!lastWinningTableFound) {
+      printer.printLine("====================")
+      val drewNumbers = allWinningNumbers.slice(0, i)
+      printer.printLine(s"Drew numbers: ${drewNumbers.mkString(", ")}")
+      printer.printLine()
+      nonWinningTables.foreach(table => {
+        val bingoTable = table.copy(drewNumbers = drewNumbers)
+        printer.printLine(bingoTable.prettyToString())
+      })
+
+      val newNonWinningTables = nonWinningTables
+        .map(table => table.copy(drewNumbers = drewNumbers))
+        .filterNot(_.isWinning())
+
+      if (newNonWinningTables.isEmpty) {
+
+        nonWinningTables match {
+          case Seq(table) =>
+            lastWinningTableFound = true
+            lastWinningTable = table.copy(drewNumbers = drewNumbers)
+            printer.printLine("Last winning table found!!")
+          case Seq(_, _*) =>
+            throw new IllegalStateException("More than one last winning table found!")
+        }
+      } else {
+        i = i + 1
+        printer.printLine("Still checking...")
+      }
+      nonWinningTables = newNonWinningTables
+      printer.printLine("====================")
+    }
+
+    (lastWinningTable, allWinningNumbers(i - 1))
   }
 
   "Part 1" should {
--- src/test/scala/year2021/Day5.scala
+++ src/test/scala/year2021/Day5.scala
@@ -3,8 +3,8 @@ package year2021
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpecLike
 import util.{Printer, Task}
-import year2021.day5.Day5Printer._
-import year2021.day5.Day5Util._
+
+import scala.math.{abs, max, signum}
 
 class Day5 extends Task(2021, 5) with AnyWordSpecLike with Matchers {
   def part1(input: Seq[String])(implicit printer: Printer): Int = {
@@ -30,6 +30,92 @@ class Day5 extends Task(2021, 5) with AnyWordSpecLike with Matchers {
 
     dangerLevels
       .count(_._2 > 1)
+  }
+
+  def getXY(input: Seq[String])(implicit printer: Printer): (Int, Int) = {
+    printer.printLine("====================")
+    val (xMax, yMax) = input
+      .foldLeft((0, 0)) {
+        case ((xMax, yMax), next) =>
+          next match {
+            case s"$x1s,$y1s -> $x2s,$y2s" =>
+              val (x1, x2, y1, y2) = (x1s.toInt, x2s.toInt, y1s.toInt, y2s.toInt)
+              (max(max(x1, x2), xMax), max(max(y1, y2), yMax))
+          }
+      }
+    printer.printLine(s"Size of the field is $xMax x $yMax")
+    printer.printLine("====================")
+    (xMax, yMax)
+  }
+
+  def getDangerousLines(input: Seq[String], withDiagonals: Boolean = false)(implicit printer: Printer): Seq[Seq[(Int, Int)]] = {
+    printer.printLine("====================")
+    val ret = input.map {
+      case line @ s"$x1s,$y1s -> $x2s,$y2s" =>
+        printer.printLine(s"Parsing $line")
+        val (x1, x2, y1, y2) = (x1s.toInt, x2s.toInt, y1s.toInt, y2s.toInt)
+        val dangerousFields = {
+          if (x1 == x2 || y1 == y2 || (withDiagonals && (x1 - x2 == y2 - y1 || x1 - x2 == y1 - y2))) {
+            val lengthOfLine = max(abs(x1 - x2) + 1, abs(y1 - y2) + 1)
+            printer.printLine(s"lengthOfLine: $lengthOfLine")
+
+            for (i <- 0 until lengthOfLine) yield {
+              val x = x1 + signum(x2 - x1) * i
+              val y = y1 + signum(y2 - y1) * i
+              printer.printLine(s"Adding: $x, $y")
+              (x, y)
+            }
+          } else {
+            Seq.empty[(Int, Int)]
+          }
+        }
+        if (dangerousFields.nonEmpty) {
+          printer.printLine(s"Dangerous fields: $dangerousFields")
+        } else {
+          printer.printLine("Skipping line...")
+        }
+        printer.printLine("--------------------")
+        dangerousFields
+    }
+    printer.printLine("====================")
+    ret.filter(_.nonEmpty)
+  }
+
+  def accumulateDangerousFields(dangerousFields: Seq[(Int, Int)])(implicit printer: Printer): Map[(Int, Int), Int] = {
+    printer.printLine("====================")
+    val accumulated = dangerousFields
+      .groupBy { case (x, y) => (x, y) }
+      .view
+      .mapValues(_.length)
+      .toMap
+    printer.printLine(s"Accumulated dangers are: $accumulated")
+    printer.printLine("====================")
+    accumulated
+  }
+
+  def printDangerousLines(dangerousLines: Seq[Seq[(Int, Int)]], xMax: Int, yMax: Int)(implicit printer: Printer): Unit = {
+    printer.ifEnabled() {
+      printer.printLine("====================")
+      printer.printLine("Dangerous lines are:")
+      dangerousLines.foreach(line => {
+        val from = line.head
+        val to   = line.last
+        printer.printLine(s"${from._1}, ${from._2} -> ${to._1}, ${to._2}")
+        printDangerLevels(accumulateDangerousFields(line), xMax, yMax)
+      })
+      printer.printLine("====================")
+    }
+  }
+
+  def printDangerLevels(dangerLevels: Map[(Int, Int), Int], xMax: Int, yMax: Int)(implicit printer: Printer): Unit = {
+    printer.ifEnabled() {
+      for (y <- 0 to yMax) yield {
+        val fieldLine = (for (x <- 0 to xMax) yield {
+          dangerLevels.getOrElse((x, y), ".").toString
+        }).mkString
+        printer.printLine(fieldLine)
+      }
+    }
   }
 
   "Part 1" should {
