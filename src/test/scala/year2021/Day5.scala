03a1d9ade5a25fc585d879d592e5857a6c0b7e2e
Sükein Alex <sukein.alex@gmail.com>
2021-12-07 20:03:00 +0100

    refactor Day5

--- src/test/scala/year2021/Day5.scala
+++ src/test/scala/year2021/Day5.scala
@@ -9,23 +9,9 @@ import scala.math.{abs, max, signum}
 class Day5 extends Task(2021, 5) with AnyWordSpecLike with Matchers {
   var printer: Printer = _
 
-  def part1(input: Seq[String]): Int = {
+  def solution(input: Seq[String], withDiagonals: Boolean): Int = {
     val (xMax, yMax)   = getXY(input)
-    val dangerousLines = getDangerousLines(input)
-
-    printDangerousLines(dangerousLines, xMax, yMax)
-
-    val dangerLevels = accumulateDangerousFields(dangerousLines.flatten)
-
-    printDangerLevels(dangerLevels, xMax, yMax)
-
-    dangerLevels
-      .count(_._2 > 1)
-  }
-
-  def part2(input: Seq[String]): Int = {
-    val (xMax, yMax)   = getXY(input)
-    val dangerousLines = getDangerousLines(input, withDiagonals = true)
+    val dangerousLines = getDangerousLines(input, withDiagonals)
     val dangerLevels   = accumulateDangerousFields(dangerousLines.flatten)
 
     printDangerLevels(dangerLevels, xMax, yMax)
@@ -57,11 +43,13 @@ class Day5 extends Task(2021, 5) with AnyWordSpecLike with Matchers {
         printer.printLine(s"Parsing $line")
         val (x1, x2, y1, y2) = (x1s.toInt, x2s.toInt, y1s.toInt, y2s.toInt)
         val dangerousFields = {
-          if (x1 == x2 || y1 == y2 || (withDiagonals && (x1 - x2 == y2 - y1 || x1 - x2 == y1 - y2))) {
-            val lengthOfLine = max(abs(x1 - x2) + 1, abs(y1 - y2) + 1)
+          val dx = abs(x1 - x2)
+          val dy = abs(y1 - y2)
+          if (x1 == x2 || y1 == y2 || (withDiagonals && (dx == dy))) {
+            val lengthOfLine = max(dx, dy)
             printer.printLine(s"lengthOfLine: $lengthOfLine")
 
-            for (i <- 0 until lengthOfLine) yield {
+            for (i <- 0 until (lengthOfLine + 1)) yield {
               val x = x1 + signum(x2 - x1) * i
               val y = y1 + signum(y2 - y1) * i
               printer.printLine(s"Adding: $x, $y")
@@ -95,20 +83,6 @@ class Day5 extends Task(2021, 5) with AnyWordSpecLike with Matchers {
     accumulated
   }
 
-  def printDangerousLines(dangerousLines: Seq[Seq[(Int, Int)]], xMax: Int, yMax: Int): Unit = {
-    printer.ifEnabled() {
-      printer.printLine("====================")
-      printer.printLine("Dangerous lines are:")
-      dangerousLines.foreach(line => {
-        val from = line.head
-        val to   = line.last
-        printer.printLine(s"${from._1}, ${from._2} -> ${to._1}, ${to._2}")
-        printDangerLevels(accumulateDangerousFields(line), xMax, yMax)
-      })
-      printer.printLine("====================")
-    }
-  }
-
   def printDangerLevels(dangerLevels: Map[(Int, Int), Int], xMax: Int, yMax: Int): Unit = {
     printer.ifEnabled() {
       for (y <- 0 to yMax) yield {
@@ -125,14 +99,14 @@ class Day5 extends Task(2021, 5) with AnyWordSpecLike with Matchers {
       printer = new Printer(true)
 
       val input = getExample()
-      part1(input) shouldBe 5
+      solution(input, withDiagonals = false) shouldBe 5
     }
 
     "solve the task" in {
       printer = new Printer(false)
 
       val input = getTask()
-      part1(input) shouldBe 3990
+      solution(input, withDiagonals = false) shouldBe 3990
     }
   }
 
@@ -141,14 +115,14 @@ class Day5 extends Task(2021, 5) with AnyWordSpecLike with Matchers {
       printer = new Printer(true)
 
       val input = getExample()
-      part2(input) shouldBe 12
+      solution(input, withDiagonals = true) shouldBe 12
     }
 
     "solve the task" in {
       printer = new Printer(false)
 
       val input = getTask()
-      part2(input) shouldBe 21305
+      solution(input, withDiagonals = true) shouldBe 21305
     }
   }
 }
